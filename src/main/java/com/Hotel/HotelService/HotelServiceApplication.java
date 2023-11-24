255b27e4d11cf2f24218b3c235e82bd965d4d100
mariem benyoussef <benyoussefmariem@bizerte.r-iset.tn>
2023-11-24 17:59:33 +0100

    api_spring

--- src/main/java/com/Hotel/HotelService/HotelServiceApplication.java
+++ src/main/java/com/Hotel/HotelService/HotelServiceApplication.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class HotelServiceApplication implements CommandLineRunner {
-
 
 	@Autowired
 	public HotelRepository hotelRepository;
@@ -27,13 +25,16 @@ public class HotelServiceApplication implements CommandLineRunner {
 		hotel1.setName("Hotel A");
 		hotel1.setAddress("123 Main Street");
 		hotel1.setRating(4);
-		hotelRepository.save(hotel1);
+		hotel1.setImageUrl("Bureau\\img.png"); // Utilisez \\ au lieu de \
 
 		Hotel hotel2 = new Hotel();
 		hotel2.setName("Hotel B");
 		hotel2.setAddress("456 Oak Avenue");
 		hotel2.setRating(3);
-		hotelRepository.save(hotel2);
+		hotel2.setImageUrl("Bureau\\img.png"); // Utilisez \\ au lieu de \
 
+		hotelRepository.save(hotel1);
+		hotelRepository.save(hotel2);
 	}
+
 }
